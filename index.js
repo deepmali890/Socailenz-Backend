c9d5bb63fe8360e062b911f1db3cc6d469d28eac
deepmali890 <deepmali590@gmail.com>
2025-06-01 22:33:59 +0530

    cors add

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ const postRoutes = require('./src/routes/post.routes');
 const messageRoutes = require('./src/routes/message.routes');
 
 const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+};
+
+app.use(cors(corsOptions));
 const server = http.createServer(app); // ✅ Wrap express in http server
 
 // ✅ Create Socket.IO server
